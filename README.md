aeece38f361d14c43e664eed97df75e28b5dd41b
overlookmotel <theoverlookmotel@gmail.com>
2016-06-18 20:48:05 +0100

    Initial release

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 # Is this a bluebird promise I see before me?
 
-## Current status
-
 [![NPM version](https://img.shields.io/npm/v/is-bluebird.svg)](https://www.npmjs.com/package/is-bluebird)
 [![Build Status](https://img.shields.io/travis/overlookmotel/is-bluebird/master.svg)](http://travis-ci.org/overlookmotel/is-bluebird)
 [![Dependency Status](https://img.shields.io/david/overlookmotel/is-bluebird.svg)](https://david-dm.org/overlookmotel/is-bluebird)
@@ -11,6 +9,60 @@
 [![Coverage Status](https://img.shields.io/coveralls/overlookmotel/is-bluebird/master.svg)](https://coveralls.io/r/overlookmotel/is-bluebird)
 
 ## Usage
+
+Tools to check whether some input is a [bluebird](http://bluebirdjs.com/) promise, a bluebird promise constructor, or determining the version of bluebird from a promise or constructor.
+
+#### `isBluebird( promise )`
+
+Returns true if is a bluebird promise, false if not.
+
+```js
+var isBluebird = require('is-bluebird');
+var Bluebird = require('bluebird');
+
+console.log( isBluebird( Bluebird.resolve() ) ); // true
+console.log( isBluebird( Promise.resolve() ) ); // false (native JS promise)
+```
+
+#### `isBluebird.ctor( Promise )`
+
+Returns true if is bluebird promise constructor, false if not.
+
+```js
+var isBluebird = require('is-bluebird');
+var Bluebird = require('bluebird');
+
+console.log( isBluebird.ctor( Bluebird ) ); // true
+console.log( isBluebird.ctor( Promise ) ); // false (native JS promise)
+```
+
+#### `isBluebird.v2( promise )` / `isBluebird.v3( promise )`
+
+Returns true if is a bluebird promise of the specified version.
+
+```js
+var isBluebird = require('is-bluebird');
+var Bluebird2 = require('bluebird2');
+var Bluebird3 = require('bluebird3');
+
+console.log( isBluebird.v2( Bluebird2.resolve() ) ); // true
+console.log( isBluebird.v2( Bluebird3.resolve() ) ); // false
+console.log( isBluebird.v2( Promise.resolve() ) ); // false (native JS promise)
+```
+
+#### `isBluebird.v2.ctor( promise )` / `isBluebird.v3.ctor( promise )`
+
+Returns true if is bluebird promise constructor of the specified version.
+
+```js
+var isBluebird = require('is-bluebird');
+var Bluebird2 = require('bluebird2');
+var Bluebird3 = require('bluebird3');
+
+console.log( isBluebird.v2.ctor( Bluebird2 ) ); // true
+console.log( isBluebird.v2.ctor( Bluebird3 ) ); // false
+console.log( isBluebird.v2.ctor( Promise ) ); // false (native JS promise)
+```
 
 ## Tests
 
--- test/all.test.js
+++ test/all.test.js
@@ -6,6 +6,8 @@
 // modules
 var chai = require('chai'),
 	expect = chai.expect,
+	Bluebird2 = require('bluebird2'),
+	Bluebird3 = require('bluebird3'),
 	isBluebird = require('../lib/');
 
 // init
@@ -16,8 +18,182 @@ chai.config.includeStack = true;
 /* jshint expr: true */
 /* global describe, it */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(isBluebird).to.be.ok;
+describe('isBluebird', function() {
+	it('returns true for bluebird v2 promise', function() {
+		expect(isBluebird(bluebird2Promise())).to.be.true;
+	});
+
+	it('returns true for bluebird v3 promise', function() {
+		expect(isBluebird(bluebird3Promise())).to.be.true;
+	});
+
+	testIfNativePromise('returns false for native JS promise', function() {
+		expect(isBluebird(nativePromise())).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		expect(isBluebird({_addCallbacks: function() {}})).to.be.false;
 	});
 });
+
+describe('isBluebird.ctor', function() {
+	it('returns true for bluebird v2 constructor', function() {
+		expect(isBluebird.ctor(Bluebird2)).to.be.true;
+	});
+
+	it('returns true for bluebird v3 constructor', function() {
+		expect(isBluebird.ctor(Bluebird3)).to.be.true;
+	});
+
+	testIfNativePromise('returns false for native JS promise constructor', function() {
+		expect(isBluebird.ctor(Promise)).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird.ctor()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird.ctor(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		var obj = {};
+		obj.prototype = {_addCallbacks: function() {}};
+		expect(isBluebird.ctor(obj)).to.be.false;
+	});
+});
+
+describe('isBluebird.v2', function() {
+	it('returns true for bluebird v2 promise', function() {
+		expect(isBluebird.v2(bluebird2Promise())).to.be.true;
+	});
+
+	it('returns false for bluebird v3 promise', function() {
+		expect(isBluebird.v2(bluebird3Promise())).to.be.false;
+	});
+
+	testIfNativePromise('returns false for native JS promise', function() {
+		expect(isBluebird.v2(nativePromise())).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird.v2()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird.v2(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		expect(isBluebird.v2({_addCallbacks: function() {}})).to.be.false;
+	});
+});
+
+describe('isBluebird.v2.ctor', function() {
+	it('returns true for bluebird v2 constructor', function() {
+		expect(isBluebird.v2.ctor(Bluebird2)).to.be.true;
+	});
+
+	it('returns false for bluebird v3 constructor', function() {
+		expect(isBluebird.v2.ctor(Bluebird3)).to.be.false;
+	});
+
+	testIfNativePromise('returns false for native JS promise constructor', function() {
+		expect(isBluebird.v2.ctor(Promise)).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird.v2.ctor()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird.v2.ctor(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		var obj = {};
+		obj.prototype = {_addCallbacks: function() {}};
+		expect(isBluebird.v2.ctor(obj)).to.be.false;
+	});
+});
+
+describe('isBluebird.v3', function() {
+	it('returns true for bluebird v3 promise', function() {
+		expect(isBluebird.v3(bluebird3Promise())).to.be.true;
+	});
+
+	it('returns false for bluebird v2 promise', function() {
+		expect(isBluebird.v3(bluebird2Promise())).to.be.false;
+	});
+
+	testIfNativePromise('returns false for native JS promise', function() {
+		expect(isBluebird.v3(nativePromise())).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird.v3()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird.v3(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		expect(isBluebird.v3({_addCallbacks: function() {}})).to.be.false;
+	});
+});
+
+describe('isBluebird.v3.ctor', function() {
+	it('returns true for bluebird v3 constructor', function() {
+		expect(isBluebird.v3.ctor(Bluebird3)).to.be.true;
+	});
+
+	it('returns false for bluebird v2 constructor', function() {
+		expect(isBluebird.v3.ctor(Bluebird2)).to.be.false;
+	});
+
+	testIfNativePromise('returns false for native JS promise constructor', function() {
+		expect(isBluebird.v3.ctor(Promise)).to.be.false;
+	});
+
+	it('returns false for undefined', function() {
+		expect(isBluebird.v3.ctor()).to.be.false;
+	});
+
+	it('returns false for null', function() {
+		expect(isBluebird.v3.ctor(null)).to.be.false;
+	});
+
+	it('returns false for object', function() {
+		var obj = {};
+		obj.prototype = {_addCallbacks: function() {}};
+		expect(isBluebird.v3.ctor(obj)).to.be.false;
+	});
+});
+
+// support functions
+function bluebird2Promise() {
+	return new Bluebird2(function() {});
+}
+
+function bluebird3Promise() {
+	return new Bluebird3(function() {});
+}
+
+function nativePromise() {
+	return new Promise(function() {});
+}
+
+function testIfNativePromise(name, fn) {
+	if (global.Promise) return it(name, fn);
+	return it.skip(name);
+}
